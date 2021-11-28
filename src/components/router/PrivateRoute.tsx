7f887c3d8afba8f9de799e4cda12f3214a26ee71
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-28 13:20:22 +0900

    211128_notice_feed_bug

--- src/components/router/PrivateRoute.tsx
+++ src/components/router/PrivateRoute.tsx
@@ -5,7 +5,6 @@ import { useAppContext } from '../../hooks/ReduserContext';
 
 // ----------PrivateRouteコンポーネントの作成(ログイン状態によるリダイレクト)----------------------
 const PrivateRoute: React.FC<RouteProps> = ({ ...props }) => {
-  /* eslint-disable */
   const { path } = props;
   /* eslint-disable */
 
@@ -15,9 +14,7 @@ const PrivateRoute: React.FC<RouteProps> = ({ ...props }) => {
   console.log(isLogin ? 'ログイン済み' : 'ログインできていない');
 
   if (isLogin) {
-    /* eslint-disable */
     return <Route {...props} />;
-    /* eslint-disable */
   } else {
     console.log(`ログインいていないユーザーは${path}へはアクセスできません`);
     return <Redirect to="/login" />;
@@ -25,3 +22,4 @@ const PrivateRoute: React.FC<RouteProps> = ({ ...props }) => {
 };
 
 export default PrivateRoute;
+/* eslint-disable */
