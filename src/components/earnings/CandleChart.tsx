44ab81225ccec7df9edc998ea280b73b519b6100
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-12-07 00:58:44 +0900

    211206_4_fix_echarts

--- src/components/earnings/CandleChart.tsx
+++ src/components/earnings/CandleChart.tsx
@@ -1,32 +1,35 @@
+/* eslint-disable */
+
 import { FC, useRef, useEffect } from 'react';
 
+import { useTheme } from '@mui/material';
 import * as echarts from 'echarts';
 import { useColors } from '../../hooks/util';
 import { StockPrice } from '../Types';
+import './dark.js';
+import './vintage.js';
 
 type PropsType = {
   historical: StockPrice[];
   symbol: string;
 };
 
-/* eslint-disable */
-
 const CandleChart: FC<PropsType> = ({ historical, symbol }) => {
+  const theme = useTheme();
   const colors = useColors();
-  // const { state } = useAppContext();
+
+  const setTheme = theme.palette.mode === 'dark' ? 'dark' : 'vintage';
+
   // const { currentUser } = state;
   console.log('historical', historical);
 
   const divRef = useRef<HTMLDivElement>(null!);
 
-  // type EChartsOption = echarts.EChartOption<Series>;
-  type EChartsOption = echarts.EChartsOption;
-
-  // if (divRef.current === null) return <div>divRefがnull</div>;
-
   useEffect(() => {
-    const { upColor, upBorderColor, downColor, downBorderColor } = colors.chart;
+    const { upColor, upBorderColor, downColor, downBorderColor, borderColor } =
+      colors.chart;
 
+    // データの整形
     const splitData = (jsonData: StockPrice[]) => {
       const categoryData: string[] = [];
       const values: number[][] = [];
@@ -52,6 +55,7 @@ const CandleChart: FC<PropsType> = ({ historical, symbol }) => {
 
     const data0 = splitData(historical);
 
+    // 移動平均線関数
     const calculateMA = (dayCount: number) => {
       const result: Array<number | '-'> = [];
 
@@ -76,329 +80,283 @@ const CandleChart: FC<PropsType> = ({ historical, symbol }) => {
       return result;
     };
 
-    const option: EChartsOption = {
-      // animation: false,
-      legend: {
-        top: 5,
-        left: 'center',
-        data: ['日足', 'MA50', 'MA200'],
-        textStyle: {
-          color: colors.text,
-        },
-      },
-      // title: {
-      //   text: symbol,
-      //   left: 0,
-      //   textStyle: {
-      //     color: colors.text,
-      //   },
-      // },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-        },
-        backgroundColor: colors.header,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10,
-        textStyle: {
-          color: colors.text,
-        },
-        position(pos, params, el, elRect, size) {
-          const obj: Record<string, number> = {
-            top: 10,
-          };
-          obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
+    const myChart = echarts.init(divRef.current);
 
-          return obj;
-        },
-      },
-      // extraCssText: 'width: 170px'
-
-      axisPointer: {
-        link: [
-          {
-            xAxisIndex: 'all',
-          },
-        ],
-        label: {
-          backgroundColor: '#777',
-        },
-      },
-      toolbox: {
-        feature: {
-          dataZoom: {
-            yAxisIndex: false,
-          },
-          brush: {
-            type: ['lineX', 'clear'],
-          },
-        },
-      },
-      brush: {
-        xAxisIndex: 'all',
-        brushLink: 'all',
-        outOfBrush: {
-          colorAlpha: 0.1,
-        },
-      },
-      visualMap: {
-        show: false,
-        seriesIndex: 5,
-        dimension: 2,
-        pieces: [
-          {
-            value: 1,
-            color: downColor,
-          },
-          {
-            value: -1,
-            color: upColor,
-          },
-        ],
-      },
-      grid: [
-        {
-          left: '10%',
-          right: '3%',
-          height: '50%',
-        },
-        {
-          left: '10%',
-          right: '3%',
-          top: '63%',
-          height: '20%',
-        },
-      ],
-      xAxis: [
-        {
-          type: 'category',
-          data: data0.categoryData,
-          scale: true,
-          boundaryGap: false,
-          axisLine: { onZero: false },
-          splitLine: { show: false },
-          min: 'dataMin',
-          max: 'dataMax',
-          axisPointer: {
-            z: 100,
-          },
-        },
-        {
-          type: 'category',
-          gridIndex: 1,
-          data: data0.categoryData,
-          scale: true,
-          boundaryGap: false,
-          axisLine: { onZero: false },
-          axisTick: { show: false },
-          splitLine: { show: false },
-          axisLabel: { show: false },
-          min: 'dataMin',
-          max: 'dataMax',
-        },
-      ],
-      yAxis: [
-        {
-          scale: true,
-          splitArea: {
-            show: true,
-          },
-        },
-        {
-          scale: true,
-          gridIndex: 1,
-          splitNumber: 2,
-          axisLabel: { show: false },
-          axisLine: { show: false },
-          axisTick: { show: false },
-          splitLine: { show: false },
-        },
-      ],
-      dataZoom: [
-        {
-          type: 'inside',
-          xAxisIndex: [0, 1],
-          rangeMode: ['value', 'value'],
-          startValue: data0.categoryData.length - 99,
-          endValue: data0.categoryData.length + 1,
-        },
-        {
-          // 横軸のズームスライダー
-          show: true,
-          xAxisIndex: [0, 1],
-          type: 'slider',
-          top: '85%',
-          rangeMode: ['value', 'value'],
-          startValue: 40,
-          endValue: 80,
+    myChart.setOption(
+      {
+        legend: {
+          top: 30,
+          left: 'center',
+          data: ['日足', 'MA50', 'MA200', '出来高'],
           textStyle: {
             color: colors.text,
           },
         },
-      ],
-      series: [
-        {
-          name: '日足',
-          type: 'candlestick',
-          data: data0.values,
-          itemStyle: {
-            color: upColor,
-            color0: downColor,
-            borderColor: upBorderColor,
-            borderColor0: downBorderColor,
+        title: {
+          text: symbol,
+          left: 0,
+          textStyle: {
+            color: colors.text,
           },
-          // tooltip: {
-          //   formatter (param: any) {
-          //     param = param[0];
-
-          //     return [
-          //       `Date: ${  param.name  }<hr size=1 style="margin: 3px 0">`,
-          //       `Open: ${  param.data[0]  }<br/>`,
-          //       `Close: ${  param.data[1]  }<br/>`,
-          //       `Lowest: ${  param.data[2]  }<br/>`,
-          //       `Highest: ${  param.data[3]  }<br/>`,
-          //     ].join('');
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+          },
+          backgroundColor: colors.header,
+          borderWidth: 1,
+          borderColor: '#ccc',
+          padding: 10,
+          textStyle: {
+            color: colors.text,
+          },
+          // position: (
+          //   pos: [number, number],
+          //   size: {
+          //     contentSize: [number, number];
+          //     viewSize: [number, number];
           //   },
-          // },
+          // ) => {
+          //   const obj: Record<string, number> = {
+          //     top: 10,
+          //   };
+          //   obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
 
-          markPoint: {
-            label: {
-              formatter(param: any) {
-                return param != null ? `${Math.round(param.value)}` : '';
+          //   return obj;
+          // },
+        },
+        axisPointer: {
+          link: [
+            {
+              xAxisIndex: 'all',
+            },
+          ],
+          label: {
+            backgroundColor: '#777',
+          },
+        },
+        toolbox: {
+          feature: {
+            show: true,
+            dataZoom: {
+              yAxisIndex: 'none',
+            },
+            dataView: { readOnly: false },
+            magicType: { type: ['line', 'bar'] },
+            brush: {
+              type: ['lineX', 'clear'],
+            },
+            restore: {},
+            saveAsImage: {},
+          },
+        },
+        brush: {
+          xAxisIndex: 'all',
+          brushLink: 'all',
+          outOfBrush: {
+            colorAlpha: 0.1,
+          },
+        },
+        visualMap: [
+          {
+            type: 'piecewise',
+            show: false,
+            seriesIndex: 2,
+            dimension: 2,
+            borderColor: 'blue',
+            pieces: [
+              {
+                value: 1,
+                color: downColor,
               },
+              {
+                value: -1,
+                color: upColor,
+              },
+            ],
+          },
+        ],
+        // visualMap: {
+        //   show: false,
+        //   seriesIndex: 5,
+        //   dimension: 2,
+        //   pieces: [
+        //     {
+        //       value: 1,
+        //       color: downColor,
+        //     },
+        //     {
+        //       value: -1,
+        //       color: upColor,
+        //     },
+        //   ],
+        // },
+        grid: [
+          {
+            left: '10%',
+            right: '3%',
+            height: '50%',
+          },
+          {
+            left: '10%',
+            right: '3%',
+            top: '63%',
+            height: '20%',
+          },
+        ],
+        xAxis: [
+          {
+            type: 'category',
+            data: data0.categoryData,
+            scale: true,
+            boundaryGap: false,
+            axisLine: {
+              onZero: false,
+              lineStyle: { color: borderColor },
+            },
+            splitLine: {
+              show: true,
+              lineStyle: { color: borderColor },
+            },
+            min: 'dataMin',
+            max: 'dataMax',
+            axisPointer: {
+              z: 100,
+            },
+          },
+          {
+            type: 'category',
+            gridIndex: 1,
+            data: data0.categoryData,
+            scale: true,
+            boundaryGap: false,
+            axisLine: { onZero: false },
+            axisTick: { show: false },
+            splitLine: { show: false },
+            axisLabel: { show: false },
+            min: 'dataMin',
+            max: 'dataMax',
+          },
+        ],
+        yAxis: [
+          {
+            scale: true,
+            axisLine: {
+              show: true,
+              lineStyle: { color: borderColor },
+            },
+            splitLine: {
+              show: true,
+              lineStyle: { color: borderColor },
+            },
+            splitArea: {
+              show: true,
+              areaStyle: {
+                color: ['none', 'none'],
+              },
+            },
+          },
+          {
+            scale: true,
+            gridIndex: 1,
+            splitNumber: 2,
+            axisLabel: { show: false },
+            axisLine: { show: false },
+            axisTick: { show: false },
+            splitLine: { show: false },
+          },
+        ],
+        dataZoom: [
+          {
+            type: 'inside',
+            xAxisIndex: [0, 1],
+            rangeMode: ['value', 'value'],
+            startValue: data0.categoryData.length - 99,
+            endValue: data0.categoryData.length + 1,
+          },
+          {
+            // 横軸のズームスライダー
+            show: true,
+            xAxisIndex: [0, 1],
+            type: 'slider',
+            top: '85%',
+            rangeMode: ['value', 'value'],
+            startValue: 40,
+            endValue: 80,
+            textStyle: {
               color: colors.text,
             },
-            data: [
-              {
-                name: 'Mark',
-                coord: ['2013/5/31', 2300],
-                value: 2300,
-                itemStyle: {
-                  color: 'red',
-                },
-              },
-              {
-                name: 'highest value',
-                type: 'max',
-                valueDim: 'highest',
-              },
-              {
-                name: 'lowest value',
-                type: 'min',
-                valueDim: 'lowest',
-              },
-              {
-                name: 'average value on close',
-                type: 'average',
-                valueDim: 'close',
-              },
-            ],
+          },
+        ],
+        series: [
+          {
+            name: '日足',
+            type: 'candlestick',
+            data: data0.values,
+            itemStyle: {
+              color: upColor,
+              color0: downColor,
+              borderColor: upBorderColor,
+              borderColor0: downBorderColor,
+            },
+
             tooltip: {
               formatter(param: any) {
-                return `${param.name}<br>${param.data.coord || ''}`;
+                param = param[0];
+
+                return [
+                  `Date: ${param.name}<hr size=1 style="margin: 3px 0">`,
+                  `Open: ${param.data[0]}<br/>`,
+                  `Close: ${param.data[1]}<br/>`,
+                  `Lowest: ${param.data[2]}<br/>`,
+                  `Highest: ${param.data[3]}<br/>`,
+                ].join('');
               },
             },
           },
-          markLine: {
-            symbol: ['none', 'none'],
-            data: [
-              [
-                {
-                  name: 'from lowest to highest',
-                  type: 'min',
-                  valueDim: 'lowest',
-                  symbol: 'circle',
-                  symbolSize: 10,
-                  label: {
-                    show: false,
-                  },
-                  emphasis: {
-                    label: {
-                      show: false,
-                    },
-                  },
-                },
-                {
-                  type: 'max',
-                  valueDim: 'highest',
-                  symbol: 'circle',
-                  symbolSize: 10,
-                  label: {
-                    show: false,
-                  },
-                  emphasis: {
-                    label: {
-                      show: false,
-                    },
-                  },
-                },
-              ],
-              {
-                name: 'min line on close',
-                type: 'min',
-                valueDim: 'close',
-                itemStyle: {
-                  color: 'red',
-                },
-              },
-              {
-                name: 'max line on close',
-                type: 'max',
-                valueDim: 'close',
-                itemStyle: {
-                  color: 'red',
-                },
-              },
-            ],
+          {
+            name: 'MA50',
+            type: 'line',
+            data: calculateMA(50),
+            smooth: true,
+            lineStyle: {
+              opacity: 0.4,
+            },
           },
-        },
-        {
-          name: 'MA50',
-          type: 'line',
-          data: calculateMA(50),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.4,
+          {
+            name: 'MA200',
+            type: 'line',
+            data: calculateMA(200),
+            smooth: true,
+            lineStyle: {
+              opacity: 0.4,
+            },
           },
-        },
-        {
-          name: 'MA200',
-          type: 'line',
-          data: calculateMA(200),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.4,
+          {
+            name: '出来高',
+            type: 'bar',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data: data0.volumes,
           },
-        },
-        {
-          name: '出来高',
-          type: 'bar',
-          xAxisIndex: 1,
-          yAxisIndex: 1,
-          data: data0.volumes,
-        },
-      ],
-    };
+        ],
+      },
+      true,
+    );
 
-    const myChart = echarts.init(divRef.current);
-
-    option && myChart.setOption(option);
-
-    myChart.dispatchAction({
-      type: 'brush',
-      areas: [
-        {
-          brushType: 'lineX',
-          coordRange: ['2021-10-10', '2021-12-02'],
-          xAxisIndex: 0,
-        },
-      ],
-    });
-    // return () => {
-    // };
-  }, [historical, symbol, colors]);
+    // myChart.dispatchAction({
+    //   type: 'brush',
+    //   areas: [
+    //     {
+    //       brushType: 'lineX',
+    //       coordRange: ['2021-10-10', '2021-12-02'],
+    //       xAxisIndex: 0,
+    //     },
+    //   ],
+    // });
+    // return () => {};
+  }, [historical, symbol, colors, theme.palette.mode]);
 
   return (
     <>
